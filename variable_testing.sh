e03a6368f5114311ee680604bb08a3165aae68a9
Daniel Hunter <officialdanielhunter@gmail.com>
2017-06-23 09:49:53 -0700

    Added printing of a random word that is specific length based on user input, using piping

--- variable_testing.sh
+++ variable_testing.sh
@@ -12,11 +12,7 @@
 # fi
 
 # Print a random word
-cat /usr/share/dict/words | sed -n "${RANDOM}p"
+# cat /usr/share/dict/words | sed -n "${RANDOM}p"
 
-# rand=$RANDOM # grab a random number
-# cat /usr/share/dict/words > test # save the dictionary into a file
-# numLines=$( wc -l < test ) # count the number of lines in the file
-# rand=$((rand%numLines)) # ensure the random number is less than the length of dictionary
-# sed -n "${rand}p" test # print a random word from the file
-# rm test # delete the file
+# Print a random word that is the length passed in as the first argument from the user
+# cat /usr/share/dict/words | grep -x ".\{${1},${1}\}" | sed -n "${RANDOM}p"
