522d4d3e18b44376fea2a5aecc52238723e00eb1
Daniel Hunter <officialdanielhunter@gmail.com>
2017-06-26 15:45:02 -0700

    Added an annotated analyze1.sh

---
+++ tldp/analyze1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Author: daniel.hunter
+# 06-26-2017
+
+DIRNAME=/usr/bin
+FILETYPE="shell script"
+LOGFILE=logfile
+
+file "$DIRNAME"/* | fgrep "$FILETYPE" | tee $LOGFILE | wc -l
+
+# Scans all files in /usr/bin
+# Sends the output of that to fgrep, which searches for everything containing the
+#   string "shell script"
+# Sends the output of that to tee, which saves the output in a file called 'logfile'
+# The output is also passed along, and then the line count is printed to stdout
+
+exit 0
