d5ae202ab099e5235e2a5c21ab33b044ca42fcc2
Daniel Hunter <officialdanielhunter@gmail.com>
2018-02-08 20:41:56 -0800

    Add shutdown after server log off and minor cleanup

--- ssh-into-aws.sh
+++ ssh-into-aws.sh
@@ -7,33 +7,39 @@ if [ "$#" -ne 1 ]; then
         exit 3
 fi
 
-if [ $1 == 'stop' ]; then
+if [ "$1" == 'stop' ]; then
     aws ec2 stop-instances --instance-ids i-016940554988ae126
     aws ec2 stop-instances --instance-ids i-088218b334cf97c8d
-    aws ec2 stop-instances --instance-ids i-0df581e7c85a4bc89
+    aws ec2 stop-instances --instance-ids i-0f0651b333034b762
     exit 0
 fi
 
-if [ $1 == 'p2' ]; then
+if [ "$1" == 'p2' ]; then
     instanceId='i-016940554988ae126'
+    user='ubuntu'
 fi
 
-if [ $1 == 't2' ]; then
+if [ "$1" == 't2' ]; then
     instanceId='i-088218b334cf97c8d'
+    user='ubuntu'
 fi
 
-if [ $1 == 'car' ]; then
-    instanceId='i-0df581e7c85a4bc89'
+if [ "$1" == 'car' ]; then
+    instanceId='i-0f0651b333034b762'
+    user='carnd'
 fi
 
 # Start the instance
 echo 'Booting up the instance...'
 aws ec2 start-instances --instance-ids $instanceId && \
 aws ec2 wait instance-running --instance-ids $instanceId && \
-instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress"`
+instanceIp=$(aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress")
 
 echo 'Waiting a few seconds to ensure full startup...'
-sleep 10 # Sleep while the instance boots
+sleep 5 # Sleep while the instance boots
 
 # # SSH into the instance
-ssh -L localhost:8888:localhost:8888 -i ~/.ssh/aws-key-fast-ai.pem ubuntu@$instanceIp
+ssh -L localhost:8888:localhost:8888 -i ~/.ssh/aws-key-fast-ai.pem $user@"$instanceIp"
+
+# When the SSH command finishes, shut down the instance
+aws ec2 stop-instances --instance-ids $instanceId
