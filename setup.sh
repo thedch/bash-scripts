a9c6a85f5d520891280718e2cb0f79051a46fed9
Daniel Hunter <officialdanielhunter@gmail.com>
2018-03-06 13:51:11 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 # Script to grab my dot files and place them in the approriate places
 # Useful for quickly setting up AWS / GCE / general VMs
 #
-# In order to run the script itself:
-# curl https://raw.githubusercontent.com/thedch/bash-scripts/master/setup.sh > ./setup.sh
+# In order to download the script:
+# curl -L setup.thedaniel.me > ./setup.sh && chmod 700 ./setup.sh && ./setup.sh
 #
 
 base_url="https://raw.githubusercontent.com/thedch/dot-files/master/"
