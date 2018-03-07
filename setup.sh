75725d9dbb05fa7351f385eb1bd0ce3bbf31af74
Daniel Hunter <officialdanielhunter@gmail.com>
2018-03-06 20:02:33 -0800

    Major overhaul to setup.sh, added error checking of typos and overwrites

--- setup.sh
+++ setup.sh
@@ -11,4 +11,27 @@ echo "What files would you like? Please enter the filename. Example: '.vimrc'"
 
 read filename
 
-curl "${base_url}""${filename}" > ~/"${filename}"
+# Check if file already exists locally
+if [ -f ~/${filename} ]; then
+    read -p "File already exists -- would you like to overwrite it? " -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "Exiting without overwriting..."
+        [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+    fi
+fi
+
+# Check if the file exists on the server
+echo "Checking file on server..."
+respCode=$(curl --head "${base_url}""${filename}" | grep HTTP | awk '{print $2}')
+# echo Your response code was $respCode
+
+if [ "$respCode" -ne "200" ]; then
+    echo 'Response code was not 200, did you make a typo?'
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+# If everything has worked, download the file and save it
+echo "Downloading file..."
+curl --silent "${base_url}""${filename}" > ~/"${filename}"
