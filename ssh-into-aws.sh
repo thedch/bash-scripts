3c08913651be0ec431b8b471b0e8091762c295a5
Daniel Hunter <officialdanielhunter@gmail.com>
2018-03-18 11:37:52 -0700

    Add instructions to modify the script for your own use

--- ssh-into-aws.sh
+++ ssh-into-aws.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
-# Author: daniel
+# Author: Daniel Hunter
 # February 06 2018
 
-T="$(date +%s)"
+##
+# Instructions: Everywhere there is an instanceID, change it to be an instanceID
+# of your own (get from AWS console). I have three different instances I use
+# frequently, so I have three different arguments. You can have as many or as few
+# as you like.
+##
+
+T="$(date +%s)" # get current time to use later
 
 if [ "$#" -ne 1 ]; then
-        echo "[ERROR] Please provide t2, p2, or car as an argument."
+        # Ensure the user provided command line arguments
+        echo "[ERROR] Please provide stop, t2, p2, or car as an argument."
         exit 3
 fi
 
 if [ "$1" == 'stop' ]; then
+    # If the command line argument was stop, shut down all instances
     aws ec2 stop-instances --instance-ids i-016940554988ae126
     aws ec2 stop-instances --instance-ids i-088218b334cf97c8d
     aws ec2 stop-instances --instance-ids i-0f0651b333034b762
     exit 0
 fi
 
+# Change 'p2', 't2', 'car' to be whatever is convenient for you -- this is purely a
+# user convenience feature
 if [ "$1" == 'p2' ]; then
     instanceId='i-016940554988ae126'
     user='ubuntu'
@@ -38,10 +49,10 @@ aws ec2 wait instance-running --instance-ids $instanceId && \
 instanceIp=$(aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress")
 
 echo 'Waiting a few seconds to ensure full startup...'
-sleep 5 # Sleep while the instance boots
+sleep 5 # Sleep while the instance boots just in case it didn't fully
 
-# # SSH into the instance
-ssh -L 8888:localhost:8888 -i ~/.ssh/aws-key-fast-ai.pem $user@"$instanceIp"
+# SSH into the instance
+ssh -L 8888:localhost:8888 -i ~/.ssh/aws-key-fast-ai.pem "$user"@"$instanceIp"
 
 # When the SSH command finishes, shut down the instance
 aws ec2 stop-instances --instance-ids $instanceId
